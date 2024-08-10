ba7031b8a3bbfb1a3ceb32f4bf898744005b04a2
Rahul-V-20 <142727705+Rahul-V-20@users.noreply.github.com>
2024-08-10 17:23:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGold: '#C89D7C',
+      },
+    },
+  },
+  plugins: [],
+}
+
